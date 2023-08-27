b99089b38ea502272b763084c4469b4cd95ed582
vivek devkar <mrvivekdevkar123@gmail.com>
2023-08-27 19:53:29 +0530

    ajax request created

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('',home,name='home'),
     path('<str:group_name>/',index,name='index'),
+    path('result/output/', Output, name='output'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,20 +1,35 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.http import JsonResponse,HttpResponse
 from .models import Group
-# Create your views here.
+import requests
+import json
+
+
+def Output(request):
+    number = [2,3,4,2,2]
+    return JsonResponse({'number':number})
+
 
 def home(request):
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         group_name = request.POST.get('group_name')
-        group = Group.objects.filter(group_name = group_name).first()
+        group = Group.objects.filter(group_name=group_name).first()
 
         if not group:
-            group = Group(group_name = group_name)
+            group = Group(group_name=group_name)
             group.save()
 
-        return redirect('index',group_name=group_name)
+        return redirect('index', group_name=group_name)
     
-    return render(request,'app/home.html')
+    return render(request, 'app/home.html')
 
-def index(request,group_name):
-    group = Group.objects.get(group_name = group_name)
-    return render(request,'app/index.html',{'groupname':group_name,'group':group})
+
+def index(request, group_name):
+    group = Group.objects.get(group_name=group_name)
+    
+    # if request.method == 'POST':
+    #     code = request.POST.get('code')
+    #     output = compile_and_run(code)
+    #     return JsonResponse({'output': output})
+    
+    return render(request, 'app/index.html', {'groupname': group_name, 'group': group})
